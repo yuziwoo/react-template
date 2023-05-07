6560553f29e16dbae1fe534664136fef8a35c1c7
Jiwoo <78391069+yuziwoo@users.noreply.github.com>
2023-05-07 12:33:26 +0900

    라이브러리 작업 준비 끝

--- src/jw/jw-library.js
+++ src/jw/jw-library.js
@@ -3,15 +3,19 @@ export class JwLibrary {
         this.device = ""; // mob, pc
         this.browser = ""; // Chorme, Firefox, Safari, Opera, Edge, Android, iPhone
 
-        let body = document.getElementById("root");
+        this.body = document.getElementById("root");
 
         this.getBrowser();
-        body.classList.add(`device-${this.device}`);
+        this.body.classList.add(`device-${this.device}`);
         // mob, pc 환경을 구분하여 root element에 클래스 추가
         // jw-pc, jw-mob 클래스를 이용하여 원하는 환경에서만 요소가 보일 수 있도록 작성해보세요.
 
-        body.classList.add(this.orientation);
+        this.body.classList.add(this.orientation);
         // 뷰포트의 방향에 따라 가로가 더 길면 landscape, 세로가 더 길면 portrait 클래스 추가
+
+        // 스크롤, 리사이즈 이벤트 생성
+        this.bindScroll = this.scroll.bind(this);
+        this.bindResize = this.resize.bind(this);
     }
 
     getBrowser() {
@@ -44,12 +48,33 @@ export class JwLibrary {
         this.orientation = window.innerWidth >= window.innerHeight ? "landscape" : "portrait";
     }
 
+    resize() {
+        if (window.innerWidth >= window.innerHeight) {
+            if (this.orientation != "landscape") {
+                this.orientation = "landscape";
+                this.body.classList.remove("portrait");
+                this.body.classList.add("landscape");
+            }
+        } else {
+            if (this.orientation != "portrait") {
+                this.orientation = "portrait";
+                this.body.classList.remove("landscape");
+                this.body.classList.add("portrait");
+            }
+        }
+    }
+    scroll() {
+        //
+    }
+    // 라이프사이클
     onStart() {
-        console.log("start")
+        window.addEventListener("resize", this.bindResize);
+        window.addEventListener("scroll", this.bindScroll);
     }
 
     onEnd() {
-        console.log("end")
+        window.removeEventListener("resize", this.bindResize);
+        window.removeEventListener("scroll", this.bindScroll);
     }
 
     cutText(text, limit) {
